f7350ee77cda04aa3c55c00d15aa25a12f600b7c
chihiro <chihiro0131@gmail.com>
2021-07-18 19:32:49 +0900

    クラス化、クエリパターン追加

--- usage/elasticsearchUsage/elasticsample.py
+++ usage/elasticsearchUsage/elasticsample.py
@@ -1,13 +1,24 @@
 from elasticsearch import Elasticsearch
+import json
 import pprint
 
 
-def essample(query):
-    es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
-    es.info()
-    result = es.search(index='amazon', body=query)
-    pprint.pprint(f'{result}')
-    es.close()
+class esclass:
+
+    def __init__(self):
+        self.es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
+        infodata = self.es.info()
+        print('-- print es.info() ----------------------------')
+        pprint.pprint(infodata, indent=2)
+
+    def __del__(self):
+        self.es.close()
+
+    def search(self, query: str):
+        return self.es.search(index='amazon', body=query)
+
+    def count(self, query: str):
+        return self.es.count(index='amazon', body=query)
 
 
 if __name__ == '__main__':
@@ -16,4 +27,19 @@ if __name__ == '__main__':
             'match_all': {}
         }
     }
-    essample(query)
+    query2 = {
+        'query': {
+            'exists': {
+                'field': 'founder'
+            }
+        }
+    }
+
+    es = esclass()
+    for item in (query, query2):
+        result = es.search(item)
+        print('-- print es.search() ----------------------------')
+        print(json.dumps(result, indent=2))
+        result = es.count(item)
+        print('-- print es.count() ----------------------------')
+        print(json.dumps(result, indent=2))
