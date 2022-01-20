18de223bc8de1cf3d14d903dc02559ebbddcd040
HiramatsuChihiro <hiramatsu.chr@ncontr.com>
2022-01-20 09:37:47 +0900

    allow dirpath.

--- usage/chardetUsage/chardetsample.py
+++ usage/chardetUsage/chardetsample.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 import os
 import chardet
+import glob
+
+
+def check_fileencode(filepath: str):
+    """chardetライブラリを使用したファイルエンコード判定
+
+    複数ファイル指定に対応したファイルエンコード判定処理
+
+    Parameters
+    ----------
+    filepath : str
+        以下いずれかの形式を期待する
+        ディレクトリ指定(./tmp)
+        拡張子指定(./tmp/*.json)
+    """
+    files = glob.glob(filepath)
+    for file in files:
+        chardet_detect(file)
+
+
+def chardet_detect(filepath: str):
+    """chardetライブラリを使用したファイルエンコード判定
+
+    Parameters
+    ----------
+    filepath : str
+        フォーマットチェック対象のファイルパス
+    """
+    print(f'target : {filepath}')
+    with open(filepath, 'rb') as r:
+        bynary = r.read()
+        print(chardet.detect(bynary))
 
 
 def main():
@@ -9,13 +41,7 @@ def main():
     filepath = os.path.join(
         OWN_FILE_DIR, *['../../', 'basic', 'data', 'json', 'not_utf8.json'])
 
-    # normal_str = 'hello'
-    # binary_str = b'\x83X\x83}\x83J\x83\x81'
-    # chardet.detect(normal_str)
-    # chardet.detect(binary_str)
-    with open(filepath, 'rb') as r:
-        bynary = r.read()
-        print(chardet.detect(bynary))
+    check_fileencode(filepath)
 
 
 if __name__ == "__main__":
