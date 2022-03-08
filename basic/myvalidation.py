453261b7338bab88c69623773eca6f55acc8470a
Hiramatsu Chihiro <hiramatsu.chr@ncontr.com>
2022-03-08 19:29:49 +0900

    add startswith usage.

--- basic/myvalidation.py
+++ basic/myvalidation.py
@@ -14,5 +14,19 @@ def typecheck():
             print(f'{data} is not str')
 
 
+def includestr():
+    """文字列の部分チェック
+
+    startswith句で部分一致のチェック
+    """
+    target = '2022/03/08 12:34:56'
+    search_str = '2022/03/08 12:34:00'
+
+    # 秒を除いた'2022/03/08 12:34'で部分チェック
+    if target.startswith(search_str[:-3]):
+        print(f'{search_str} は {target} に含まれる')
+
+
 if __name__ == '__main__':
     typecheck()
+    includestr()
